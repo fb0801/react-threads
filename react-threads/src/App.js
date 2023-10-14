8bf2077ad1b505c77f5fc10f382deced78b53dc1
fb0801 <fbhatti0801@gmail.com>
2023-10-14 14:47:10 +0100

    nav url

--- react-threads/src/App.js
+++ react-threads/src/App.js
@@ -1,16 +1,37 @@
+import {useState, useEffect} from 'react'
 import Nav from './Componenets/Nav'
 import Header from './Componenets/Header'
 import Feed from './Componenets/Feed'
 import PopUp from './Componenets/PopUp'
 
 const App = () => {
+    const userID = ""
+    const [user, setUser] = useState(null)
+    const getUser = async () => {
+    try {
+      const response = await fetch(`http://localhost:3000/users?user_uuid=${}`)
+      const data = await response.json()
+      setUser(data[0])
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    getUser()
+  }, [])
+
   return (
-    <div className="app">
-    <Nav/>
-    <Header/>
+    <>
+    {user && <div className="app">
+    <Nav url={user.instagram_url}/>
+    <Header
+    user={user}
+    />
     <Feed/>
     <PopUp/>
     </div>
+    </>
   );
 }
 
